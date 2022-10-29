af9baa90220b0c04b9dec3496e0848788b16b489
Dany Giguere <danygiguere@Danys-MacBook-Pro.local>
2022-10-29 15:39:29 -0400

    Adding to pokemon test

--- test/controllers/pokemons_controller_test.rb
+++ test/controllers/pokemons_controller_test.rb
@@ -35,8 +35,26 @@ class PokemonsControllerTest < ActionDispatch::IntegrationTest
     assert_response :redirect
     follow_redirect!
     assert_response :success
-    get pokemon_url(@pokemon)
-    assert_response :success
+  end
+
+  test "should note create pokemon when missing name" do
+    post pokemons_url,
+           params: {
+             pokemon: {
+              name:       "",
+              type1:      @pokemon.type1,
+              type2:      @pokemon.type2,
+              total:      @pokemon.total,
+              hp:         @pokemon.hp,
+              attack:     @pokemon.attack,
+              defense:    @pokemon.defense,
+              sp_atk:     @pokemon.sp_atk,
+              sp_def:     @pokemon.sp_def,
+              speed:      @pokemon.speed,
+              generation: @pokemon.generation,
+             },
+           }
+    assert_response :unprocessable_entity
   end
 
   test "should show pokemon" do
@@ -67,6 +85,26 @@ class PokemonsControllerTest < ActionDispatch::IntegrationTest
             },
           }
     assert_redirected_to pokemon_url(@pokemon)
+  end
+
+  test "should not update pokemon when missing name" do
+    put pokemon_url(@pokemon),
+          params: {
+            pokemon: {
+              name:       "",
+              type1:      @pokemon.type1,
+              type2:      @pokemon.type2,
+              total:      @pokemon.total,
+              hp:         @pokemon.hp,
+              attack:     @pokemon.attack,
+              defense:    @pokemon.defense,
+              sp_atk:     @pokemon.sp_atk,
+              sp_def:     @pokemon.sp_def,
+              speed:      @pokemon.speed,
+              generation: @pokemon.generation,
+            },
+          }
+    assert_response :unprocessable_entity
   end
 
   test "should destroy pokemon" do
